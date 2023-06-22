b166047c0ade0c10b2ca8eb4e451f39c51ecd764
Antonella Avogadro Amadei <antonellaa@microsoft.com>
2023-06-22 10:21:20 -0700

    testing multiline

--- Powershell/runcommand.ps1
+++ Powershell/runcommand.ps1
@@ -13,10 +13,23 @@ param(
 # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3
 # Also, note that this will run powershell.exe
 # even if the system has pwsh.exe.
-powershell.exe -Command $command
+
+# Write the contents of the command string to a temporary file and append the extension .ps1 to it
+$commandFile = [System.IO.Path]::GetTempFileName() + ".ps1"
+Write-Output "Writing command to $commandFile"
+$command | Out-File -FilePath $commandFile -Encoding ascii
+
+# run the command file using powershell.exe
+Write-Output "Running command $commandFile"
+powershell.exe -File $commandFile
 $commandExitCode = $LASTEXITCODE
 Write-Output "Command exited with code $commandExitCode"
+
+# Clean up the command file
+Write-Output "Deleting command file $commandFile"
+# Remove-Item -Path $commandFile
 
 # Task powershell scripts should always end with an
 # exit code reported up to the runner agent.
 exit $commandExitCode
+
