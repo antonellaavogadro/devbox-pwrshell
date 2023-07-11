275d894facc2aa763a970198c44a89617337bc79
Antonella Avogadro Amadei <antonellaa@microsoft.com>
2023-07-11 09:19:28 -0700

    latest choco updates to test

--- ChocoTask/choco.ps1
+++ ChocoTask/choco.ps1
@@ -35,7 +35,10 @@ function Ensure-Chocolatey
 
     if (-not (Test-Path "$ChocoExePath"))
     {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        $installScript = (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+        $expression = "$installScript | powershell.exe -NoProfile -ExecutionPolicy Bypass -Command -"
+        Execute -Expression $expression
         if ($LastExitCode -eq 3010)
         {
             Write-Host 'The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.'
@@ -68,44 +71,48 @@ function Install-Packages
         if ($PackageVersions.ContainsKey($name)) { # Check if the version was defined
             $version = "--version " + $PackageVersions[$name]
         }
-        $expression = "$ChocoExePath install -y -f --acceptlicense --ignore-checksums --no-progress --stoponfirstfailure $AdditionalOptions $name $version" # Change the command to install packages and versions
-        Invoke-ExpressionImpl -Expression $expression
+        $expression = "$ChocoExePath install -y -f --acceptlicense --no-progress --stoponfirstfailure $AdditionalOptions $name $version" # Change the command to install packages and versions
+        Execute -Expression $expression
     }
 }
 
 # Install-Packages -ChocoExePath "C:\ProgramData\Chocolatey\choco.exe" -Packages "package1, package2" -AdditionalOptions "--ignore-dependencies" -PackageVersions @{ 'package1'='1.0.1'; 'package2'='2.3.4' }
 
-function Invoke-ExpressionImpl
+function Execute
 {
     [CmdletBinding()]
     param(
         $Expression
     )
 
-    # This call will normally not throw. So, when setting -ErrorVariable it causes it to throw.
-    # The variable $expError contains whatever is sent to stderr.
-    iex $Expression -ErrorVariable expError
-
+    # Note we're calling powershell.exe directly, instead
+    # of running Invoke-Expression, as suggested by
+    # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3
+    # Note that this will run powershell.exe
+    # even if the system has pwsh.exe.
+    $process = Start-Process powershell.exe -ArgumentList "-Command $Expression" -NoNewWindow -PassThru -Wait
+    $expError = $process.ExitCode.Exception
     # This check allows us to capture cases where the command we execute exits with an error code.
     # In that case, we do want to throw an exception with whatever is in stderr. Normally, when
     # Invoke-Expression throws, the error will come the normal way (i.e. $Error) and pass via the
     # catch below.
-    if ($LastExitCode -or $expError)
+    if ($process.ExitCode -or $expError)
     {
-        if ($LastExitCode -eq 3010)
+        if ($process.ExitCode -eq 3010)
         {
             # Expected condition. The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.
         }
-        elseif ($expError[0])
+        elseif ($expError)
         {
-            throw $expError[0]
+            throw $expError
         }
         else
         {
-            throw "Installation failed ($LastExitCode). Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details."
+            throw "Installation failed ($process.ExitCode). Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details."
         }
     }
 }
+
 
 ###################################################################################################
 #
