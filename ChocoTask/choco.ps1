94f3837ba7df6faa7cae96c2e613975420b185ae
Antonella Avogadro Amadei <antonellaa@microsoft.com>
2023-08-07 13:18:40 -0700

    error handling for package installation

--- ChocoTask/choco.ps1
+++ ChocoTask/choco.ps1
@@ -12,8 +12,6 @@ param(
 
 Start-Transcript -Path C:\PerfLogs\mfastatus-result.log -Append
 
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1
-
 if (-not $Package) {
     throw "Package parameter is mandatory. Please provide a value for the Package parameter."
 }
@@ -39,19 +37,23 @@ function Ensure-Chocolatey
     [CmdletBinding()]
     param(
         [string] $ChocoExePath
-    ) 
+    )
 
     if (-not (Test-Path "$ChocoExePath"))
     {
         Set-ExecutionPolicy Bypass -Scope Process -Force
-        $installScriptPath = Join-Path $env:TEMP "Choco-Install.ps1"
+        $installScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
         Invoke-WebRequest -Uri 'https://chocolatey.org/install.ps1' -OutFile $installScriptPath
 
         try {
-            powershell.exe -NoProfile -ExecutionPolicy Bypass -File $installScriptPath
-
+            Execute -File $installScriptPath
         } finally {
-            # Remove-Item $installScriptPath
+            Remove-Item $installScriptPath
+        }
+        
+        if ($LastExitCode -eq 3010)
+        {
+            Write-Host 'The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.'
         }
     }
 }
@@ -78,14 +80,22 @@ function Install-Package
         $expression = "$expression --ignorechecksums"
     }
 
-    powershell.exe $expression
+    $expression = "$expression `nexit `$LASTEXITCODE"
+
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    $packageScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
+    Set-Content -Value $expression -Path $packageScriptPath
+    Write-Host "File path $packageScriptPath"
+
+    Execute -File $packageScriptPath
+    Remove-Item $packageScriptPath
 }
 
 function Execute
 {
     [CmdletBinding()]
     param(
-        $Expression
+        $File
     )
 
     # Note we're calling powershell.exe directly, instead
@@ -93,16 +103,18 @@ function Execute
     # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3
     # Note that this will run powershell.exe
     # even if the system has pwsh.exe.
-    $process = Start-Process powershell.exe -ArgumentList "-Command $Expression" -NoNewWindow -PassThru -Wait
-    $expError = $process.ExitCode.Exception
-    
+    powershell.exe -File $File -NoProfile -NonInteractive -NoLogo
+
+    # capture the exit code from the process
+    $processExitCode = $LASTEXITCODE
+
     # This check allows us to capture cases where the command we execute exits with an error code.
     # In that case, we do want to throw an exception with whatever is in stderr. Normally, when
     # Invoke-Expression throws, the error will come the normal way (i.e. $Error) and pass via the
     # catch below.
-    if ($process.ExitCode -or $expError)
+    if ($processExitCode -or $expError)
     {
-        if ($process.ExitCode -eq 3010)
+        if ($processExitCode -eq 3010)
         {
             # Expected condition. The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.
         }
@@ -112,7 +124,8 @@ function Execute
         }
         else
         {
-            throw "Installation failed ($LastExitCode). Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details."
+            throw "Installation failed with exit code: $processExitCode. Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details."
+            break
         }
     }
 }
