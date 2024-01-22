5e3cb8e635476e54c77ddd19c761e6af777f5675
Antonella Avogadro <122132762+antonellaavogadro@users.noreply.github.com>
2024-01-22 11:53:04 -0800

    Update calc.ps1

--- CalculatorCommand/calc.ps1
+++ CalculatorCommand/calc.ps1
@@ -1,1 +1,10 @@
-calc
+param (
+    [Parameter()]
+    [string]$CloneUri
+)
+
+$CustomizationScriptsDir = "C:\DevBoxCustomizations"
+$RunAsUserScript = "runAsUser.ps1"
+
+Add-Content -Path "$($CustomizationScriptsDir)\$($RunAsUserScript)" -Value "cd C:\"
+Add-Content -Path "$($CustomizationScriptsDir)\$($RunAsUserScript)" -Value "git clone $($CloneUri)"
